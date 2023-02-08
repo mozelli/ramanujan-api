185f5cf9917221232fd31fc1034e7b7a591690e0
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-08 13:05:36 -0300

    create posts routes

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ require('dotenv').config({ path: ".env.develop" });
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const posts = require('./routes/posts');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-require('./models/Posts');
+app.use("/posts", posts.routes);
 
 app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
