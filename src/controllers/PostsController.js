5f9f5fdbfa4c58283516c1f54cb5533d02464606
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-09 09:30:46 -0300

    catch status into request body

--- src/controllers/PostsController.js
+++ src/controllers/PostsController.js
@@ -2,13 +2,14 @@ const Posts = require('../models/Posts');
 
 module.exports = {
   async createPost(request, response) {
-    const {title, content, category, tags} = request.body;
+    const {title, content, category, status, tags} = request.body;
 
     try {
       const newPost = await Posts.create({
         title,
         category,
         tags, 
+        state: status,
         text: content
       });
       return response.status(201).json(newPost);
