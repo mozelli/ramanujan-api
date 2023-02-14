60703c851fe0c7baa3c7a4c7d8c2acaa1ca4e507
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-14 08:16:42 -0300

    include posts/list and category/create routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,4 +4,8 @@ const PostsController = require("../controllers/PostsController");
 
 routes.post("/create", (request, response) => PostsController.createPost(request, response));
 
+routes.get("/list", (request, response) => PostsController.listPosts(request, response));
+
+routes.post("/category/create", (request, response) => PostsController.createCategory(request, response));
+
 module.exports = { routes };
