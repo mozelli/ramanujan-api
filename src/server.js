6a2e5619fa25c87a7a80c143b148d52e46aa4fe5
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-14 08:13:59 -0300

    implements cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,14 @@ const posts = require('./routes/posts');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization")
+  app.use(cors());
+  next();
+});
+
 app.use("/posts", posts.routes);
 
 app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
