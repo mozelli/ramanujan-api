4b57794c9471220666dc48ad1f1e7a6562a8b08d
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-14 17:51:56 -0300

    create routes for Categories

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -8,4 +8,10 @@ routes.get("/list", (request, response) => PostsController.listPosts(request, re
 
 routes.post("/category/create", (request, response) => PostsController.createCategory(request, response));
 
+routes.get("/category/list", (request, response) => PostsController.listCategories(request, response));
+
+routes.put("/category/:id", (request, response) => PostsController.updateCategory(request, response));
+
+routes.delete("/category/:id", (request, response) => PostsController.deleteCategory(request, response));
+
 module.exports = { routes };
