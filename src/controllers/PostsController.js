68c9ce412ddaba25575c657427c01e65121714c7
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-14 08:18:56 -0300

    create listPosts and createCategory functions

--- src/controllers/PostsController.js
+++ src/controllers/PostsController.js
@@ -1,8 +1,9 @@
 const Posts = require('../models/Posts');
+const Categories = require('../models/Categories');
 
 module.exports = {
   async createPost(request, response) {
-    const {title, content, category, status, tags} = request.body;
+    const {title, content, category, status, tags, featuredImgURL} = request.body;
 
     try {
       const newPost = await Posts.create({
@@ -10,11 +11,35 @@ module.exports = {
         category,
         tags, 
         state: status,
-        text: content
+        text: content,
+        featuredImage: featuredImgURL
       });
       return response.status(201).json(newPost);
     } catch (error) {
-      return response.json(error);
+      return response.status(500).json(error);
+    }
+  },
+
+  async listPosts(request, response) {
+    try {
+      const postsList = await Posts.find().sort("-createdAt");
+      return response.status(200).json(postsList);
+    } catch (error) {
+      return response.status(500).json(error);
+    }
+  },
+  
+  async createCategory(request, response) {
+    const [name, ascending] = request.body;
+
+    try {
+      const newCategory = await Categories.create({
+        name,
+        ascending
+      });
+      return response.status(201).json(newCategory);
+    } catch(error) {
+      return response.status(500).json(error);
     }
   }
 }
